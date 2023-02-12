7f4595d448508cc78650de2b02522fd08091f202
barnardweb <125195034+barnardweb@users.noreply.github.com>
2023-02-12 11:23:56 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#navbarload").load('navbar.html')
+});
+
+console.time('cache');
+const list = $('#list');
+for (let i=0; i < 1000; i++) {
+	list.append (i);
+}
+console.timeEnd('cache')
