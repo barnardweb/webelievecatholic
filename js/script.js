f295c1c1089920f9ab1e3b8034668340087622ba
barnardweb <125195034+barnardweb@users.noreply.github.com>
2023-02-12 22:26:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@ $(document).ready(function() {
     $("#navbarload").load('./navbar.html')
 });
 
-console.time('cache');
-const list = $('#list');
-for (let i=0; i < 1000; i++) {
-	list.append (i);
-}
-
-
-console.timeEnd('cache')
